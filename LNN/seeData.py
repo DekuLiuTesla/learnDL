618c2072a7b34e41f7c9fd2adab9d554e945395e
Liu Yang <1369939686@qq.com>
2021-10-16 21:55:52 +0800

    Add code for dataset reading

---
+++ LNN/seeData.py
@@ -0,0 +1,76 @@
+import torch
+import torchvision
+from torch.utils import data
+from torchvision import transforms
+from d2l import torch as d2l
+
+
+def get_fashion_minist_label(labels):
+    text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
+                   'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+    return [text_labels[int(i)] for i in labels]
+
+
+def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):
+    figsize = (num_cols * scale, num_rows * scale)
+    _, axes = d2l.plt.subplots(num_rows, num_cols, figsize=figsize)  # figsize控制全图的长宽，单位为英寸
+    axes = axes.flatten()  # 将n*m的坐标组展平为1*nm的坐标组， 便于枚举，否则必须用二维数组引用
+    for i, (ax, img) in enumerate(zip(axes, imgs)):
+        if torch.is_tensor(img):
+            ax.imshow(img.numpy())
+        else:
+            ax.imshow(img)
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+        if titles:
+            ax.set_title(titles[i])
+
+    d2l.plt.show()
+    return axes
+
+
+def get_dataloader_workers():
+    return 4
+
+
+def load_data_fashion_mnist(batch_size, resize=None):
+    trans = [transforms.ToTensor()]  # 列表形式存储transform对象
+    if resize:
+        trans.insert(0, transforms.Resize(resize))  # Resize()方法将图片宽度等比放缩到指定大小，insert在列表第一个位置插入这个新的transform方式
+    trans = transforms.Compose(trans)  # 将多个transform对象合并在一起，相当于先转为tensor再resize
+    mnist_train = torchvision.datasets.FashionMNIST(
+        root="../data", train=True, transform=trans, download=True)
+    mnist_test = torchvision.datasets.FashionMNIST(
+        root="../data", train=False, transform=trans, download=True)
+
+    return (data.DataLoader(mnist_train, batch_size, shuffle=True,
+                            num_workers=get_dataloader_workers()),
+            data.DataLoader(mnist_test, batch_size, shuffle=False,
+                            num_workers=get_dataloader_workers()))
+
+
+trans = torchvision.transforms.ToTensor()
+mnist_train = torchvision.datasets.FashionMNIST(
+    root="../data", train=True, transform=trans, download=True)
+mnist_test = torchvision.datasets.FashionMNIST(
+    root="../data", train=False, transform=trans, download=True)
+
+# print(len(mnist_train), ", ", len(mnist_test))
+# print(mnist_train[0][0].shape)  # 第一个维度是样本序号，第二个维度中0为图像，1为标签
+# print(mnist_train[0][1])
+
+X, y = next(iter(data.DataLoader(mnist_train, batch_size=18)))
+show_images(X.reshape(18, 28, 28), 2, 9, titles=get_fashion_minist_label(y))
+
+batch_size = 256
+
+# train_iter = data.DataLoader(mnist_train, batch_size, shuffle=True, num_workers=get_dataloader_workers())
+# timer = d2l.Timer()
+# for X, y in train_iter:
+#     continue
+# print(f"{timer.stop():.2f} sec")
+
+train_iter, test_iter = load_data_fashion_mnist(32, resize=64)
+for X, y in train_iter:
+    print(X.shape,',', X.dtype, '/', y.shape, ',', y.dtype)
+    break
