d5def8f90f982538ad33cc2d00b5d176a6688c67
Liu Yang <1369939686@qq.com>
2022-02-23 11:02:17 +0800

    Added code and question answer for 13.6

--- FrequencyNet/test_CIFAR10.py
+++ FrequencyNet/test_CIFAR10.py
@@ -60,12 +60,11 @@ def train_with_data_aug(train_aug, test_aug, net, batch_size, num_epochs, lr=0.0
 def resnet_block(input_channels, num_channels, num_residuals, first_block=False, h=0, w=0):
     blk = []
     for i in range(num_residuals):
-        if i == 0:
-            stride = 2 if not first_block else 1
-            blk.append(ResFreq_gf(input_channels, num_channels,
-                                  use_1x1conv=True, strides=stride, h=h, w=w))
+        if i == 0 and not first_block:
+            blk.append(Residual_Freq(input_channels, num_channels,
+                                     use_1x1conv=True, strides=2, h=h, w=w))
         else:
-            blk.append(ResFreq_gf(num_channels, num_channels))
+            blk.append(Residual_Freq(num_channels, num_channels))
     return blk
 
 
