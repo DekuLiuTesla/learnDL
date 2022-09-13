488411548cb238f23eeb14423c1626a73656d58e
Liu Yang <1369939686@qq.com>
2022-09-13 15:26:42 +0800

    dealt with novel list downloading and ConnectionResetError: [WinError 10054]

--- NLP-inclass/text_collection.py
+++ NLP-inclass/text_collection.py
@@ -1,14 +1,62 @@
+import os
+import time
+import socket
+import random
 import requests
 from bs4 import BeautifulSoup
+from progressbar import *
 
-headers = {'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
-                         " ""Chrome/78.0.3904.70 Safari/537.36", }  # including infos about agent browser
-url = 'http://www.quanben.io/n/douluodalu/1.html'  # target website
+
+def geturl():
+    url = 'https://www.xuanshu.com/book/23011/'  # target website
+    headers = {'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
+                             " ""Chrome/78.0.3904.70 Safari/537.36", }  # including infos about agent browser
+    req = requests.get(url=url, headers=headers)
+    req.encoding = "UTF-8"  # Encoding mode
+    html = req.text
+    bes = BeautifulSoup(html, "lxml")
+    texts = bes.find("div", class_="pc_list")  # find url list for each chapter
+    chapters = texts.find_all("a")  # label "a" records corresponding url for chapter in href propety
+    websites = []
+    # Extract url for each chapter
+    for chapter in chapters:
+        name = chapter.string
+        href = chapter.get("href")
+        url_chapter = href if url in href else url + href
+        website = [url_chapter, name]  # stored in a list
+        websites.append(website)
+    return websites
+
+
+def get_txt(url, caption, headers, save_pth, **kwargs):
+    # Many retries to deal with ConnectionResetError: [WinError 10054]
+    try:
+        path = save_pth + caption.replace(" ", '_') + ".txt"
+        if not os.path.exists(path):
+            requests.adapters.DEFAULT_RETRIES = 5
+            s = requests.session()
+            s.keep_alive = False
+            response = s.get(url, headers=headers)
+            response.encoding = "UTF-8"  # deal with messy code
+            soup = BeautifulSoup(response.text, 'lxml')
+            texts = soup.find("div", kwargs)  # extract specific infos
+            texts_list = texts.text.replace(u'\u3000', u''). \
+                replace('\r', '').replace(" ", '').split("\xa0" * 4)
+            with open(path, "w") as file:
+                for line in texts_list:
+                    file.write(line + "\n")
+            response.close()
+    except:
+        time.sleep(5)
+        get_txt(url, caption, headers, save_pth, **kwargs)
+
 
 if __name__ == '__main__':
-    response = requests.request("GET", url, headers=headers)
-    response.encoding = response.apparent_encoding  # deal with messy code
-    soup = BeautifulSoup(response.text, 'lxml')
-    texts = soup.find("div", id="content")  # extract specific infos
-    texts_list = texts.text.split("\xa0" * 4)
-    print(texts_list)
+    targets = geturl()
+    headers = {'user-agent': "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) "
+                             "Gecko/20100101 Firefox/34.0"}  # including infos about agent browser
+    save_dir = "./texts/DouLuoDaLu/"
+    pbar = ProgressBar().start()
+    for i, tar in enumerate(targets):
+        get_txt(tar[0], tar[1], headers, save_dir, id="content1")
+        pbar.update(int(((i + 1) / len(targets)) * 100))
