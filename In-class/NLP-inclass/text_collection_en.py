ad0cc0c4ca5ff5b4069d123b1a521a807b1d3510
Liu Yang <1369939686@qq.com>
2022-09-23 09:16:44 +0800

    Adjusted structure of files

--- In-class/NLP-inclass/text_collection_en.py
+++ In-class/NLP-inclass/text_collection_en.py
@@ -14,14 +14,14 @@ def get_url(url):
     req.encoding = "UTF-8"  # Encoding mode
     html = req.text
     bes = BeautifulSoup(html, "lxml")
-    texts = bes.find("div", class_="pc_list")  # find url list for each chapter
+    texts = bes.find("div", class_="chapter_list")  # find url list for each chapter
     chapters = texts.find_all("a")  # label "a" records corresponding url for chapter in href propety
     websites = []
     # Extract url for each chapter
     for chapter in chapters:
         name = chapter.string
         href = chapter.get("href")
-        url_chapter = url + href
+        url_chapter = href
         website = [url_chapter, name]  # stored in a list
         websites.append(website)
     return websites
@@ -36,14 +36,13 @@ def get_txt(url, caption, headers, save_pth, **kwargs):
             s = requests.session()
             s.keep_alive = False
             response = s.get(url, headers=headers)
-            response.encoding = "UTF-8"  # deal with messy code
+            response.encoding = "gbk"  # deal with messy code.
             soup = BeautifulSoup(response.text, 'lxml')
-            texts = soup.find("div", kwargs)  # extract specific infos
-            texts_list = texts.text.replace(u'\u3000', u''). \
-                replace('\r', '').replace(" ", '').split("\xa0" * 4)
+            texts = soup.find("div", class_="chapter-lan-en toleft")  # extract specific infos
+            texts_list = texts.text.split("\xa0" * 4)
             with open(path, "w") as file:
                 for line in texts_list:
-                    file.write(line + "\n")
+                    file.write(line.replace(u'\xa0', u'') + "\n")
             response.close()
     except:
         time.sleep(5)
@@ -51,13 +50,13 @@ def get_txt(url, caption, headers, save_pth, **kwargs):
 
 
 if __name__ == '__main__':
-    save_dir = "./texts/DouLuoDaLu2/"
-    target_url = 'https://www.xuanshu.com/book/23099/'  # target website
+    save_dir = "texts/TheShawshankRedemption/"
+    target_url = 'https://www.24en.com/novel/lizhi/the-shawshank-redemption.html'  # target website
     targets = get_url(target_url)
     headers = {'user-agent': "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) "
                              "Gecko/20100101 Firefox/34.0"}  # including infos about agent browser
     pbar = ProgressBar().start()
     for i, tar in enumerate(targets):
-        file_name = f'chapter_{i}' + ".txt"
-        get_txt(tar[0], file_name, headers, save_dir, id="content1")
+        file_name = f'chapter_{i}'
+        get_txt(tar[0], file_name, headers, save_dir, class_="chapter-lan-en toleft")
         pbar.update(int(((i + 1) / len(targets)) * 100))
